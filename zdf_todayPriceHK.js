6029ae3dfe6b09e31da79bf18004bb336beea3f3
null <summer19940609@gmail.com>
2025-01-08 10:26:32 +0800

    Create zdf_todayPriceHK.js

---
+++ zdf_todayPriceHK.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const notify = require('./sendNotify');
+
+const getGoldPrice = async () => {
+  try {
+    const timestamp = Date.now();
+    const response = await axios.get(`https://api2.ctfmall.com/gateway//ctfmall-common2-server/common/todayGoldPrice?timestamp=${timestamp}`);
+    
+    const todayPriceHK = response.data.data.todayPriceHK;
+    console.log('Today\'s HK Gold Price:', todayPriceHK);
+    
+    const str = `周大福今日金价：${todayPriceHK} 元/克`
+    await notify.sendNotify(str, '');
+
+  } catch (error) {
+    console.error('Error fetching gold price:', error.message);
+    throw error;
+  }
+}
+
+getGoldPrice();
